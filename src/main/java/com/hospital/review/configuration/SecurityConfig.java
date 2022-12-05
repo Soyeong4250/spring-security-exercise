6164aa5596c620018319b0ab7b20ac2ff6d52d04
이소영 <th42500@naver.com>
2022-12-05 11:37:58 +0900

    [Refactor] SecurityConfig - 모든 Post 요청 막기

--- src/main/java/com/hospital/review/configuration/SecurityConfig.java
+++ src/main/java/com/hospital/review/configuration/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hospital.review.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -18,8 +19,8 @@ public class SecurityConfig {
                 .csrf().disable()
                 .cors().and()
                 .authorizeRequests()
-                .antMatchers("/api/**").permitAll()
                 .antMatchers("/api/v1/users/join", "/api/v1/users/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
                 .and()
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
